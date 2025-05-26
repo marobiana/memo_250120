efd61f2b53917d57e6afbab10f105bc092479478
test <test@test.com>
2025-05-26 18:45:41 +0900

    회원가입, 로그인 마크업

--- src/main/java/com/memo/user/UserController.java
+++ src/main/java/com/memo/user/UserController.java
@@ -15,4 +15,13 @@ public class UserController {
     public String signUpView() {
         return "user/signUp";
     }
+
+    /**
+     * 로그인 화면
+     * @return
+     */
+    @GetMapping("/sign-in-view")
+    public String signInView() {
+        return "user/signIn";
+    }
 }
